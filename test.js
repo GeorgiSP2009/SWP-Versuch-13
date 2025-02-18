7c2ab8a9c92b101c328f36c0033a535eed29658a
GeorgiSP2009 <DIM230092@spengergasse.at>
2025-02-18 14:29:21 +0100

    Commiting 18.02.2025

---
+++ test.js
@@ -0,0 +1,11 @@
+cars.forEach(function(car) {
+    console.log(car);
+  });
+  
+let cars = ["VW", "Tesla", "Toyota", "Buick"];
+
+for (let i = cars.length - 1; i >= 0; i--) {
+  console.log(cars[i]);
+}
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Kiwi");
